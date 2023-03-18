a76354a3b89dcf3d6856bb022f0a2137e652ad66
Rodrigo Pavon <rodrigopavongomez@gmail.com>
2023-03-18 09:23:32 -0300

    delete files and imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./App.scss";
-import ScrollToTop from "./Helpers";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
